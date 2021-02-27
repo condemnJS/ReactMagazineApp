5ed7fb19786532d00ddb286de138dade1fe0d346
condemnJS <gizat.sultan@mail.ru>
2021-02-27 13:06:16 +0300

    dev

--- src/pages/CategoryItem.js
+++ src/pages/CategoryItem.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useCallback, useState, useEffect} from 'react';
+import {useFetch} from "../hooks/useFetch";
 
-export const CategoryItem = () => {
+export const CategoryItem = (props) => {
+    const [categories, setCategories] = useState([]);
+    const locationHref = props.match.params.slug // Наш текущий slug в url, с помощью его будем делать запроос на серверc
+    const {request} = useFetch();
+    const getCategoriesAndSub = useCallback(async () => {
+        try {
+            const data = await request(
+                `http://magazine.test/api/categories/${locationHref}`
+            );
+            setCategories(data);
+
+        } catch (e) {
+            console.log(e);
+        }
+    }, []);
+    useEffect(() => {
+        getCategoriesAndSub();
+    }, [])
+    console.log(categories);
     return (
-        <h1>Niigger</h1>
+        <div style={{zIndex: -1}}>
+        </div>
     )
 }
